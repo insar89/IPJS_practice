e3ecc401957d14e3def5d51c56751dad71f0a3ce
insar89 <insar21@gmail.com>
2022-05-04 20:10:45 +0300

    0.01

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+
 'use strict';
-let a
-console.log(a)
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let lastMovie0 = prompt('Один из последних просмотренных фильмов?', '');
+let lastMovieScore0 = +prompt('На сколько оцените его?', '');
+let lastMovie1 = prompt('Один из последних просмотренных фильмов?', '');
+let lastMovieScore1 = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies = {
+  [lastMovie0]: lastMovieScore0,
+  [lastMovie1]: lastMovieScore1,
+};
+
+console.log(personalMovieDB);
